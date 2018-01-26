746593da38f0735b0e0e2e9c09454ac24713aea5
Victor Prokopiev <v.prokopev@corp.mail.ru>
2018-01-26 13:34:50 +0300

    comments + skipping SELECT

--- aofpipeprefixer.go
+++ aofpipeprefixer.go
@@ -20,6 +20,8 @@ func main() {
 		}
 		if op1.Command == "SELECT" {
 			currentDatabase = op1.Arguments[0]
+			//Databases are not supported - skipping command
+			continue
 		}
 		if op1.Key != "" {
 			if op1.Key == "sf_prof_index" {
@@ -27,6 +29,7 @@ func main() {
 			}
 			op1.Key = currentDatabase + ":" + op1.Key
 		}
+		//Databases are not supported - dangerous commands
 		if op1.Command == "FLUSHDB" || op1.Command == "FLUSHALL" {
 			continue
 		}
